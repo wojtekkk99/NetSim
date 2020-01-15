8834dcfc14c88d3ca0527c33c659e638a108a465
Ksycior <49477726+Ksycior@users.noreply.github.com>
2020-01-15 11:30:19 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,8 +3,14 @@
 //
 
 #include "include/nodes.hpp"
+#include "factory.hpp"
+double numm() {
+    return 0.5;
+}
 
 int main() {
+    std::string t = "WORKER id=3 delivery-interval=5";
+    auto p = parse_line(t);
     return 0;
 }
 
